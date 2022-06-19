26ab28944ee01fe412517ab2ec25a4620474e46d
Zain Ahmed <zain.ahmed@backcountry.com>
2022-06-19 20:58:22 +0500

    update code for react hook workshop

--- src/App.js
+++ src/App.js
@@ -1,32 +1,8 @@
 import logo from "./logo.svg";
 import "./App.css";
-import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
-import Lecture_3 from "./Lecture_3";
-import Lecture_4 from "./Lecture_4";
-import Lecture_1 from "./Lecture_1";
-import Lecture_2 from "./Lecture_2";
 
 function App() {
-  return (
-    <Lecture_4/>
-    // <Lecture_3/>
-    // <div className="App">
-    //   <header className="App-header">
-    //     <img src={logo} className="App-logo" alt="logo" />
-    //     <p>
-    //       Edit <code>src/App.js</code> and save to reload.
-    //     </p>
-    //     <a
-    //       className="App-link"
-    //       href="https://reactjs.org"
-    //       target="_blank"
-    //       rel="noopener noreferrer"
-    //     >
-    //       Learn React
-    //     </a>
-    //   </header>
-    // </div>
-  );
+  return <p></p>;
 }
 
 export default App;
